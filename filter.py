9202b413192a9e713683eef7dad0a4247b6fc082
Richard Gan <richardgan@Richards-MacBook-Air.local>
2023-08-26 20:58:30 +1000

    finished filter.py

--- filter.py
+++ filter.py
@@ -8,19 +8,26 @@ Implements the filter functionality for the website
 
 import pandas as pd
 
-
-charities = pd.read_csv("FinalCharity.csv")
-
 # List of all possible categories
 CAUSES = ["Animals", "Education", "Health", "Environment", "Human rights", "Aboriginal", "Adults", "Elderly",
           "Children", "LGBTQ+", "Refugees/Migrants", "Homeless", "Chronic Illness", "Disabilities", "Unemployed",
           "Veterans"]
+# List of all possible needs
 NEEDS = ["Finance", "Food", "Clothes", "Electronics", "Blood", "Nut juice", "Slave labour"]
 
-causes_selected = ["Education", "Disabilities"]  # Values are for testing only
-needs_selected = ["Finance, Food"]  # Values are for testing only
 
-filtered_charities = charities[charities[causes_selected].any(axis=1) & charities[needs_selected].any(axis=1)]
+def filter_by_category(charities: pd.DataFrame, causes_selected: list, needs_selected):
+    filtered_charities = charities[charities[causes_selected].any(axis=1) & charities[needs_selected].any(axis=1)]
+    return filtered_charities
+
+
+charities = pd.read_csv("FinalCharity1.csv")
+
+causes_selected = ["Education", "Disabilities"]  # Values are for testing only
+needs_selected = ["Finance", "Food"]  # Values are for testing only
+
+# filtered_charities = charities[charities[causes_selected].any(axis=1) & charities[needs_selected].any(axis=1)]
+filtered_charities = filter_by_category(charities, causes_selected, needs_selected)
 
 print(filtered_charities)
 
