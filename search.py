36820b3d03a9f52ceb47cc652ec30e63441d631d
nguyendang2004 <104487618+nguyendang2004@users.noreply.github.com>
2023-08-27 10:09:06 +1000

    Add files via upload

--- search.py
+++ search.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from enum import Enum
 
-
+from flask import Flask, render_template
 # Enum class for 'sort by' filter
 class Sorting(Enum):
     RATING_DESC = 1  # Sort first by rating from highest to lowest, then by most to least reviews
@@ -16,47 +16,42 @@ class Sorting(Enum):
     ALPHA_AZ = 3  # Sort in alphabetic order, A to Z
     ALPHA_ZA = 4  # Sort in reverse alphabetic order, Z to A
 
-
-def search(search_string: str, charities: pd.DataFrame, sort_by: Sorting):
-    names = charities["Charity_Legal_Name"]
-    results = charities.loc[names.str.contains(search_string, case=False, regex=False)].copy()
-    sort_results(results, sort_by)
-    return results
-
-
 def sort_results(results: pd.DataFrame, sort_by: Sorting):
     if sort_by == Sorting.RATING_DESC:
         # Sort first by rating from highest to lowest, then by most to least reviews
-        results.sort_values(by=["Rating", "Num_Reviews"], ascending=False, inplace=True)
+        results.sort_values(by=["Rating", "Rating_count"], ascending=False, inplace=True)
 
     elif sort_by == Sorting.RATING_ASC:
         # Sort first by rating from lowest to highest, then by most to least reviews
-        results.sort_values(by=["Rating", "Num_Reviews"], ascending=[True, False], inplace=True)
+        results.sort_values(by=["Rating", "Rating_count"], ascending=[True, False], inplace=True)
 
     else:
         a_to_z = True if sort_by == Sorting.ALPHA_AZ else False
         results.sort_values(by=["Charity_Legal_Name"], ascending=a_to_z, inplace=True)
 
 
-"""
-TESTING ONLY
-"""
-
-charities = pd.read_csv("FinalCharity.csv")
+charities = pd.read_csv("FinalCharity1.csv")
 
 # Arbitrarily add RNG Rating and Num_Reviews column for demonstration/testing
-np.random.seed(0)
-mean_rating = 4
-sd_rating = 1
-random_ratings = np.random.normal(mean_rating, sd_rating, len(charities))
-charities["Rating"] = np.clip(random_ratings, a_min=0, a_max=5).round(1)
 
-mean_num_reviews = 200
-sd_num_reviews = 100
-random_num_reviews = np.random.normal(mean_num_reviews, sd_num_reviews, len(charities))
-charities["Num_Reviews"] = np.clip(random_num_reviews, a_min=0, a_max=1000).astype(int)
 
-string = input("Search: ")
-results = search(string, charities, Sorting.RATING_DESC)
+search = input("Search: ")
+names = charities["Charity_Legal_Name"]
+results = charities.loc[names.str.contains(search, case=False, regex=False)].copy()
+
+sort_by = Sorting.RATING_DESC  # What to sort results by
+sort_results(results, sort_by)
 
 print(results)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    numb = len(results)
+    return render_template('result.html', number=numb)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
