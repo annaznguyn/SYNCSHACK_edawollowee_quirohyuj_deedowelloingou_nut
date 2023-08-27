11e4d889630937cbd75614be555dc20c04cb3742
Anna Nguyen <xngu8505@uni.sydney.edu.au>
2023-08-27 11:17:00 +1000

    Updated homepage html and python files

--- website.py
+++ website.py
@@ -2,8 +2,11 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        selected_category = request.form.get('category')
+        print(f'You selected: {selected_category}')
     return render_template('homepage.html')
 
 @app.route('/login')
